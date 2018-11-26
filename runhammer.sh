dfbc3d91f8c54b9a23024c1e09504b3bdef68732
nqdtan <tan.nqd@gmail.com>
2018-11-26 14:17:52 -0800

    updated hammer script

--- runhammer.sh
+++ runhammer.sh
@@ -2,8 +2,20 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+VERILOG_MODULE=$1
+
+if [ -z "$VERILOG_MODULE" ]
+then
+  echo "Need a verilog module"
+  exit
+fi
+
+VERILOG_TOP=${VERILOG_MODULE%.v}
+
 export HAMMER_HOME=$DIR/hammer
 
 source $HAMMER_HOME/sourceme.sh
 
-cd "$DIR" && hammer-vlsi synthesis -o syn-output.json -v AESTopCREECBus.v --top AESTopCREECBus -p config/clocks.json -p config/use_vivado.json  --obj_dir out
+cd "$DIR" && hammer-vlsi synthesis -o syn-output.json -v ${VERILOG_MODULE} --top ${VERILOG_TOP} -p config/clocks.json -p config/use_vivado.json  --obj_dir out_${VERILOG_TOP}
+
+vivado -mode batch -source extract_vivado_report.tcl -tclargs ${VERILOG_TOP}
