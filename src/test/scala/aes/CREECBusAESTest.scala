301dc1b3664121958653a4bc1fc82cafccce1020
Vighnesh Iyer <vighnesh.iyer@gmail.com>
2018-11-26 12:24:45 -0800

    aes sw model now conforms, working on hw model

--- src/test/scala/aes/CREECBusAESTest.scala
+++ src/test/scala/aes/CREECBusAESTest.scala
@@ -5,152 +5,111 @@ import interconnect.CREECAgent._
 import interconnect.{SoftwareModel, CREECHighLevelTransaction, CREECLowLevelTransaction, BusParams, CREECHeaderBeat, CREECDataBeat}
 import org.scalatest.FlatSpec
 
-class CREECBusAESSWTest extends FlatSpec with ChiselScalatestTester {
-  val testerArgs = Array(
-    "-fiwv",
-    "--backend-name", "treadle",
-    "--tr-write-vcd",
-    "--target-dir", "test_run_dir/creec",
-    "--top-name")
-
+// TODO: move the common hand-written data stimulus and golden reference to a common object
+class CREECBusAESSWTest extends FlatSpec {
   //Key is fixed in the SW model
   //TODO: Consider how to provide the model the key at time of test
-
   "AESSWModel" should "encrypt low" in {
-    implicit val busParams: BusParams = new AESBusParams
-    val enlowmodel = new CREECEncryptLowModel(busParams)
-    val id = 0
-    //Index 0 of the seq is the LSB
-    val data = Seq(1, 1, 1, 1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1, 3, 3, 2).map(
-                _.asInstanceOf[Byte])
+    implicit val busParams = new AESBusParams
+    val aesEncryptModel = new CREECEncryptLowModel(busParams)
+    val data = Seq(
+      1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 3, 3, 2).map(_.asInstanceOf[Byte])
 
-    val encrypted_data = Seq(0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
-                        0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c).map(
-                        _.asInstanceOf[Byte])
+    val encryptedData = Seq(
+      0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
+      0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c).map(_.asInstanceOf[Byte])
 
-    val out = enlowmodel.pushTransactions(Seq(
-      CREECHeaderBeat(len=1, id=id, addr=0x0),
-      CREECDataBeat(data= data, id=id),
-    )).advanceSimulation(true).pullTransactions()
+    val out = aesEncryptModel.processTransactions(Seq(
+      CREECHeaderBeat(len=1, id=0, addr=0x0),
+      CREECDataBeat(data=data, id=0),
+    ))
     val outGold = Seq(
-      CREECHeaderBeat(len=1, id=id, addr=0x0),
-      CREECDataBeat(data=encrypted_data, id=id),
+      CREECHeaderBeat(len=1, id=0, addr=0x0, encrypted=true),
+      CREECDataBeat(data=encryptedData, id=0),
     )
     assert(outGold == out)
   }
 
   "AESSWModel" should "decrypt low" in {
-    implicit val busParams: BusParams = new AESBusParams
-    val delowmodel = new CREECDecryptLowModel(busParams)
-    val id = 0
-    //Index 0 of the seq is the LSB
-    val decrypted_data = Seq(1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 3, 3, 2).map(
-      _.asInstanceOf[Byte])
+    implicit val busParams = new AESBusParams
+    val aesDecryptModel = new CREECDecryptLowModel(busParams)
+    val decryptedData = Seq(
+      1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 3, 3, 2).map(_.asInstanceOf[Byte])
 
-    val data = Seq(0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
-      0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c).map(
-      _.asInstanceOf[Byte])
+    val data = Seq(
+      0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
+      0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c).map(_.asInstanceOf[Byte])
 
-    val out = delowmodel.pushTransactions(Seq(
-      CREECHeaderBeat(len=1, id=id, addr=0x0),
-      CREECDataBeat(data= data, id=id),
-    )).advanceSimulation(true).pullTransactions()
+    val out = aesDecryptModel.processTransactions(Seq(
+      CREECHeaderBeat(len=1, id=0, addr=0x0),
+      CREECDataBeat(data= data, id=0),
+    ))
     val outGold = Seq(
-      CREECHeaderBeat(len=1, id=id, addr=0x0),
-      CREECDataBeat(data=decrypted_data, id=id),
+      CREECHeaderBeat(len=1, id=0, addr=0x0),
+      CREECDataBeat(data=decryptedData, id=0),
     )
     assert(outGold == out)
   }
 
   "AESSWModel" should "encrypt high" in {
-    implicit val busParams: BusParams = new AESBusParams
-    val enhighmodel = new CREECEncryptHighModel(busParams)
-    val id = 0
-    //Index 0 of the seq is the LSB
-    val data = Seq(1, 1, 1, 1, 1, 1, 1, 1,
+    val aesEncryptModel = new CREECEncryptHighModel
+    val data = Seq(
+      1, 1, 1, 1, 1, 1, 1, 1,
       1, 1, 1, 1, 1, 3, 3, 2,
       1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 3, 3, 2).map(
-      _.asInstanceOf[Byte])
+      1, 1, 1, 1, 1, 3, 3, 2).map(_.asInstanceOf[Byte])
 
-    val encrypted_data = Seq(0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
+    val encryptedData = Seq(
+      0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
       0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c,
       0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
-      0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c).map(
-      _.asInstanceOf[Byte])
+      0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c).map(_.asInstanceOf[Byte])
 
-    val out = enhighmodel.pushTransactions(Seq(
-      CREECHighLevelTransaction(
-        data, 0x0
-      ))).advanceSimulation(true).pullTransactions()
-    val outGold = Seq(
-      CREECHighLevelTransaction(
-        encrypted_data, 0x0
-      )
-    )
+    val out = aesEncryptModel.processTransactions(Seq(CREECHighLevelTransaction(data = data, addr = 0x0)))
+    val outGold = Seq(CREECHighLevelTransaction(data = encryptedData, addr = 0x0, encrypted = true))
     assert(outGold == out)
   }
 
   "AESSWModel" should "decrypt high" in {
-    implicit val busParams: BusParams = new AESBusParams
-    val dehighmodel = new CREECDecryptHighModel(busParams)
-    val id = 0
-    //Index 0 of the seq is the LSB
-    val decrypted_data = Seq(1, 1, 1, 1, 1, 1, 1, 1,
+    val aesDecryptModel = new CREECDecryptHighModel
+    val decryptedData = Seq(
+      1, 1, 1, 1, 1, 1, 1, 1,
       1, 1, 1, 1, 1, 3, 3, 2,
       1, 1, 1, 1, 1, 1, 1, 1,
-      1, 1, 1, 1, 1, 3, 3, 2).map(
-      _.asInstanceOf[Byte])
+      1, 1, 1, 1, 1, 3, 3, 2).map(_.asInstanceOf[Byte])
 
-    val data = Seq(0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
+    val data = Seq(
+      0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
       0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c,
       0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
-      0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c).map(
-      _.asInstanceOf[Byte])
+      0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c).map(_.asInstanceOf[Byte])
 
-    val out = dehighmodel.pushTransactions(Seq(
-      CREECHighLevelTransaction(
-        data, 0x0
-      ))).advanceSimulation(true).pullTransactions()
-    val outGold = Seq(
-      CREECHighLevelTransaction(
-        decrypted_data, 0x0
-      )
-    )
+    val out = aesDecryptModel.processTransactions(Seq(CREECHighLevelTransaction(data = data, addr = 0x0, encrypted = true)))
+    val outGold = Seq(CREECHighLevelTransaction(data = decryptedData, addr = 0x0, encrypted = false))
     assert(outGold == out)
   }
 
-  "AESSWModel" should "compose" in {
-    implicit val busParams: BusParams = new AESBusParams
-
-    val data = Seq(0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
+  "AESSWModel encrypt->decrypt chain" should "compose" in {
+    val data = Seq(
+      0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
       0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c,
       0x14, 0x23, 0x6b, 0xd1, 0xce, 0x59, 0x26, 0xe1,
-      0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c).map(
-      _.asInstanceOf[Byte])
+      0x38, 0xad, 0x15, 0x85, 0x82, 0xd4, 0x5c, 0x3c).map(_.asInstanceOf[Byte])
 
-    //Composed model
-    val composedmodel =
-      new CREECEncryptHighModel(busParams) ->
-        new CREECDecryptHighModel(busParams)
+    // Composed model
+    val composedModel =
+      new CREECEncryptHighModel ->
+      new CREECDecryptHighModel
 
-    val out = composedmodel.pushTransactions(Seq(
-      CREECHighLevelTransaction(
-        data, 0x0
-      ))).advanceSimulation(true).pullTransactions()
-    val outGold = Seq(
-      CREECHighLevelTransaction(
-        data, 0x0
-      )
-    )
+    val out = composedModel.processTransactions(Seq(CREECHighLevelTransaction(data = data, addr = 0x0)))
+    val outGold = Seq(CREECHighLevelTransaction(data = data, addr = 0x0))
     assert(outGold == out)
-
   }
 }
 
-
+/*
 class CREECBusAESHWTest extends FlatSpec with ChiselScalatestTester {
   val testerArgs = Array(
     "-fiwv",
@@ -249,3 +208,4 @@ class CREECBusAESHWTest extends FlatSpec with ChiselScalatestTester {
     }
   }
 }
+*/
