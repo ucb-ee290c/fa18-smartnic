93bb286a6d29d8e046a8c3eeb3e5b306691fcb97
nqdtan <tan.nqd@gmail.com>
2018-11-26 15:24:49 -0800

    this works better

--- runhammer.sh
+++ runhammer.sh
@@ -2,20 +2,21 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-VERILOG_MODULE=$1
+VERILOG_FILE=$1
 
-if [ -z "$VERILOG_MODULE" ]
+if [ -z "$VERILOG_FILE" ]
 then
-  echo "Need a verilog module"
+  echo "Need a verilog file"
   exit
 fi
 
+VERILOG_MODULE=$(basename ${VERILOG_FILE})
 VERILOG_TOP=${VERILOG_MODULE%.v}
 
 export HAMMER_HOME=$DIR/hammer
 
 source $HAMMER_HOME/sourceme.sh
 
-cd "$DIR" && hammer-vlsi synthesis -o syn-output.json -v ${VERILOG_MODULE} --top ${VERILOG_TOP} -p config/clocks.json -p config/use_vivado.json  --obj_dir out_${VERILOG_TOP}
+cd "$DIR" && hammer-vlsi synthesis -o syn-output.json -v ${VERILOG_FILE} --top ${VERILOG_TOP} -p config/clocks.json -p config/use_vivado.json  --obj_dir out_${VERILOG_TOP}
 
 vivado -mode batch -source extract_vivado_report.tcl -tclargs ${VERILOG_TOP}
