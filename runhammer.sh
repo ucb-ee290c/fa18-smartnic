5a4254f5d052d3d47dc908a643dff1c155ded3da
nqdtan <tan.nqd@gmail.com>
2018-11-29 11:02:36 -0800

    minor update

--- runhammer.sh
+++ runhammer.sh
@@ -20,11 +20,13 @@ fi
 VERILOG_MODULE=$(basename ${VERILOG_FILE})
 VERILOG_TOP=${VERILOG_MODULE%.v}
 
+OUTDIR=out_${VERILOG_TOP}_${FPGA_CONFIG}
+
 export HAMMER_HOME=$DIR/hammer
 
 source $HAMMER_HOME/sourceme.sh
 
-cd "$DIR" && hammer-vlsi synthesis -o syn-output.json -v ${VERILOG_FILE} --top ${VERILOG_TOP} -p config/clocks.json -p config/use_vivado.json  -p config/${FPGA_CONFIG}.yml --obj_dir out_${VERILOG_TOP}_${FPGA_CONFIG}
+cd "$DIR" && hammer-vlsi synthesis -o syn-output.json -v ${VERILOG_FILE} --top ${VERILOG_TOP} -p config/clocks.json -p config/use_vivado.json  -p config/${FPGA_CONFIG}.yml --obj_dir ${OUTDIR}
 
 source config/vivado_setup.sh
-vivado -mode batch -source extract_vivado_report.tcl -tclargs ${VERILOG_TOP}
+vivado -mode batch -source extract_vivado_report.tcl -tclargs ${OUTDIR}
