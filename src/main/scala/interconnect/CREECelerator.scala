6e073c7141ae734160d5bc0665176dc839868143
Tan Nguyen <tan.nqd@gmail.com>
2018-11-30 01:09:57 -0800

    minor change

--- src/main/scala/interconnect/CREECelerator.scala
+++ src/main/scala/interconnect/CREECelerator.scala
@@ -18,15 +18,21 @@ class CREECeleratorWrite extends Module {
   implicit val compressorBus: BusParams = creecBusParams
 
   val compressor = Module(new Compressor(io.in.p, true))
+
   val padder = Module(new CREECPadder(creecBusParams, 16))
+
   val widthExpander = Module(new CREECWidthConverter(p1 = creecBusParams,
                                                      p2 = aesBusParams))
+
   val aes = Module(new AESTopCREECBus(aesBusParams))
+
   val widthContractor1 = Module(new CREECWidthConverter(p1 = aesBusParams,
                                                         p2 = creecBusParams))
+
   val eccEncoder = Module(new ECCEncoderTop(RSParams.RS16_8_8,
                                             creecBusParams,
                                             eccBusParams))
+
   val widthContractor2 = Module(new CREECWidthConverter(p1 = eccBusParams,
                                                         p2 = creecBusParams))
 
@@ -68,7 +74,7 @@ class CREECeleratorRead extends Module {
   val widthExpander2 = Module(new CREECWidthConverter(p1 = creecBusParams,
                                                       p2 = aesBusParams))
 
-  val aesDecryption = Module(new AESTopCREECBus(aesBusParams))
+  val aes = Module(new AESTopCREECBus(aesBusParams))
 
   val widthContractor = Module(new CREECWidthConverter(p1 = aesBusParams,
                                                        p2 = creecBusParams))
@@ -80,16 +86,16 @@ class CREECeleratorRead extends Module {
   widthExpander1.io.slave <> io.in
   eccDecoder.io.slave <> widthExpander1.io.master
   widthExpander2.io.slave <> eccDecoder.io.master
-  aesDecryption.io.decrypt_slave <> widthExpander2.io.master
-  widthContractor.io.slave <> aesDecryption.io.decrypt_master
+  aes.io.decrypt_slave <> widthExpander2.io.master
+  widthContractor.io.slave <> aes.io.decrypt_master
   stripper.io.slave <> widthContractor.io.master
   decompressor.io.in <> stripper.io.master
   io.out <> decompressor.io.out
 
-  aesDecryption.io.encrypt_slave.header.noenq()
-  aesDecryption.io.encrypt_slave.data.noenq()
-  aesDecryption.io.encrypt_master.header.nodeq()
-  aesDecryption.io.encrypt_master.data.nodeq()
+  aes.io.encrypt_slave.header.noenq()
+  aes.io.encrypt_slave.data.noenq()
+  aes.io.encrypt_master.header.nodeq()
+  aes.io.encrypt_master.data.nodeq()
 }
 
 object CREECeleratorApp extends App {
